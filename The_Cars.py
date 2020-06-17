e90bf28e332c09b1545aa4f9d560f1bcbf2448a7
Lezzon <voddol69@gmail.com>
2020-06-17 14:41:41 +0300

    First change in the cars project

--- The_Cars.py
+++ The_Cars.py
@@ -1,6 +1,8 @@
 cars = ["Volvo", "Ford", "Pegeout", "Toyota"]
 if "Lada" in cars:
     print("Yes, it is")
+if "Ferrari" in cars:
+    print("Yes, it is")
 elif "Ford" in cars:
     print("\"Ford\" is in")    
 else:
